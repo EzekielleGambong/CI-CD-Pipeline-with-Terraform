d4222a8eb6bd21e13473903ee556c97df195e181
Ezekielle Louise Marie L. Gambong <ezegambong@gmail.com>
2024-11-15 23:39:02 +0800

    initial code

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "google" {
+  credentials = file("credentials.json") 
+  project     = "cloudengrprojs"            
+  region      = "us-central1"                   
+}
+
+resource "google_storage_bucket" "my-bucket" {
+  name          = "demobucket_withterra"
+  location      = "US"
+  force_destroy = true
+
+  public_access_prevention = "enforced"
+}
