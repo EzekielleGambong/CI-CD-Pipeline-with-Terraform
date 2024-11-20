9efe3ee68e7ce4f2e1f89a2abdd3d8d5f0e9de1d
Ezekielle Louise Marie L. Gambong <ezegambong@gmail.com>
2024-11-20 10:54:15 +0800

    second commit

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 provider "google" {
-  project     = "your-project-id"
-  region      = "your-region"
-  credentials = file("credentials.json")
+  project     = "cloudengrprojs"
+  region      = "us-central1"
+  credentials = file("./credentials.json")
 }
+
 
 resource "google_storage_bucket" "my-bucket" {
   name          = "demobucket_withterra"
